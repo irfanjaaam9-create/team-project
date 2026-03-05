1a7125c6de2e035481f50060472e03c534aaf6ba
irfan fareed <irfanjaaam9@gmail.com>
2026-03-05 23:11:22 +0500

    Implemented login feature

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hello Project Team")
+def login(username, password):
+    if username == "admin" and password == "1234":
+        return "Login successful"
+    else:
+        return "Login failed"
 
+print(login("admin","1234"))
